18e95e35da889369ccecb61b1951f6f3dc47bf3b
noumvar <305643392+noumvar@users.noreply.github.com>
2026-08-06 19:49:02 +0900

    変数、 if文まで

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("こんにちは");
+
+document.getElementById("tagline").innerText = "JavaScriptから書き換えました!";
+
+const siteName = "noumvar";
+console.log(siteName);
+
+let score = 50;
+
+if (score >= 60) {
+    console.log("合格です");
+} else {
+    console.log("不合格です");
+}
+
+let answer = "4";
+
+if (answer === "5") {
+    console.log("正解です");
+} else {
+    console.log("不正解です");
+}
